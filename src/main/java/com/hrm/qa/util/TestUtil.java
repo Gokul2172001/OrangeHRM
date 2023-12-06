757d5d9ea7828eeda940dbafe26f99743687a5fe
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-12-06 23:26:35 +0530

    Update TestUtil.java

--- src/main/java/com/hrm/qa/util/TestUtil.java
+++ src/main/java/com/hrm/qa/util/TestUtil.java
@@ -1,15 +1,10 @@
 package com.hrm.qa.util;
-
 import com.hrm.qa.base.TestBase;
 
 public class TestUtil extends TestBase {
-	
 	public static long PAGE_LOAD_TIMEOUT =20;
 	public static long PAGE_IMPLICIT_WAIT = 10;
-	
 	public void switchToFrame() {
-		driver.switchTo().frame("mainpanel"); 
-		
+		driver.switchTo().frame("mainpanel"); 		
 	}
 }
-
