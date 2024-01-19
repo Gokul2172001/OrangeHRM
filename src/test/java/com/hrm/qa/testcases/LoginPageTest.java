e6d8e6f86f28ca6019e5c591652a5a935f057f7e
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2024-01-19 23:31:30 +0530

    Update LoginPageTest.java

--- src/test/java/com/hrm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/hrm/qa/testcases/LoginPageTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.hrm.qa.base.TestBase;
 import com.hrm.qa.pages.DashboardPage;
 import com.hrm.qa.pages.LoginPage;
