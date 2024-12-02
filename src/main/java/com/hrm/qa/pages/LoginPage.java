d9944e8a4335b6cdc3174d2d426132cd035ec1eb
Gokul21122001 <gokul.elumalai12@gmail.com>
2024-12-02 23:50:14 +0530

    Update LoginPage.java

--- src/main/java/com/hrm/qa/pages/LoginPage.java
+++ src/main/java/com/hrm/qa/pages/LoginPage.java
@@ -6,37 +6,41 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.hrm.qa.base.TestBase;
 
-public class LoginPage extends TestBase{
-	
-	@FindBy(name="username")
+public class LoginPage extends TestBase {
+
+	@FindBy(name = "username")
 	WebElement Username;
-	
-	@FindBy(name="password")
+
+	@FindBy(name = "password")
 	WebElement Password;
-	
-	@FindBy(css="button.oxd-button")
+
+	@FindBy(css = "button.oxd-button")
 	WebElement lgnbtn;
-	
-	@FindBy(css="div.orangehrm-login-branding")
+
+	@FindBy(css = "div.orangehrm-login-branding")
 	WebElement logo;
-	
+
 	public LoginPage() {
 		PageFactory.initElements(driver, this);
 	}
+
 	public String validatePageTitle() {
 		return driver.getTitle();
 	}
+
 	public boolean checkLogo() {
 		boolean flag = logo.isDisplayed();
 		return flag;
 	}
+
 	public DashboardPage checkLogin(String user, String pwd) {
 		Username.sendKeys(user);
 		Password.sendKeys(pwd);
 		lgnbtn.click();
 		return new DashboardPage();
 	}
-	public DashboardPage homeLogoCheck() {
+
+	public boolean homeLogoCheck() {
 		return logo.isDisplayed();
 	}
 }
